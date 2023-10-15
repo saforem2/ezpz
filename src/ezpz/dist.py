eb27b58cecc6fb226580c39751a4eb533c1e1252
Sam Foreman <saforem2@gmail.com>
2023-10-15 12:01:42 -0500

    Update `dist.py`

--- src/ezpz/dist.py
+++ src/ezpz/dist.py
@@ -509,9 +509,41 @@ def mpi_test_framework_backend(
     run_mpiexec(py_cmd)
 
 
+def check(
+        framework: str = 'pytorch',
+        backend: str = 'deepspeed',
+        port: int | str = '5432'
+):
+    if framework == 'pytorch':
+        _ = setup_torch(
+            backend=backend,
+            port=port,
+        )
+    elif framework == 'tensorflow':
+        _ = setup_tensorflow()
+    else:
+        raise ValueError
+    # WORLD_SIZE = get_world_size()
+    # print(f'{RANK} / {WORLD_SIZE}')
+
+
+
 
 if __name__ == '__main__':
-    from ezpz import test
+    # from ezpz import test
+    # import sys
+    # try:
+    #     framework = sys.argv[1]
+    # except IndexError:
+    #     framework = 'pytorch'
+    # try:
+    #     backend = sys.argv[2]
+    # except IndexError:
+    #     backend = 'deepspeed'
+    # try:
+    #     port = sys.argv[3]
+    # except IndexError:
+    #     port = '5432'
     import sys
     try:
         framework = sys.argv[1]
@@ -525,3 +557,4 @@ if __name__ == '__main__':
         port = sys.argv[3]
     except IndexError:
         port = '5432'
+    check(framework=framework, backend=backend, port=port)
