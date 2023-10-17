e3b91c6733618e96235b0aa33ccb08673417c56c
Sam Foreman <saforem2@gmail.com>
2023-10-16 23:14:28 -0500

    Update `bin/setup.sh`

--- src/ezpz/bin/setup.sh
+++ src/ezpz/bin/setup.sh
@@ -11,12 +11,12 @@ while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
     [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-
 PARENT=$(dirname "${DIR}")
-MAIN="${PARENT}/main.py"
 GRANDPARENT=$(dirname "${PARENT}")
 ROOT=$(dirname "${GRANDPARENT}")
-
+MAIN="$PARENT/train.py"
+SETUP_SCRIPT="$DIR/setup.sh"
+TRAIN_SCRIPT="$DIR/train.sh"
 NCPUS=$(getconf _NPROCESSORS_ONLN)
 
 
@@ -47,6 +47,22 @@ function loadCondaEnv() {
     fi
 }
 
+function setupPython() {
+    local conda_date=$1
+    local venv_path=$2
+    if [[ "${CONDA_EXE}" ]]; then
+        echo "Caught CONDA_EXE: ${CONDA_EXE}"
+    else
+        loadCondaEnv "${conda_date}"
+    fi
+    if [[ "${VIRTUAL_ENV}" ]]; then
+        echo "Caught VIRTUAL_ENV: ${VIRTUAL_ENV}"
+    else
+        setupVenv "${venv_path}"
+    fi
+
+}
+
 # ┏━━━━━━━━━━┓
 # ┃ ThetaGPU ┃
 # ┗━━━━━━━━━━┛
@@ -57,13 +73,16 @@ function setupThetaGPU() {
         HOSTFILE=${HOSTFILE:-${COBALT_NODEFILE}}
         CONDA_DATE="2023-01-11"
         VENV_DIR="${ROOT}/venvs/${MACHINE}/${CONDA_DATE}"
-        loadCondaEnv "${CONDA_DATE}"
-        setupVenv "${ROOT}/venvs/thetaGPU/2023-01-11"
+        setupPython "${CONDA_DATE}" "${VENV_DIR}"
+        # loadCondaEnv "${CONDA_DATE}"
+        # setupVenv "${ROOT}/venvs/thetaGPU/2023-01-11"
         # Distributed setup information
         NHOSTS=$(wc -l < "${HOSTFILE}")
         NGPU_PER_HOST=$(nvidia-smi -L | wc -l)
         NGPUS=$((NHOSTS * NGPU_PER_HOST))
-        alias mpilaunch="$(which mpirun) -n $NGPUS -N $NGPU_PER_HOST --hostfile $HOSTFILE} -x PATH -x LD_LIBRARY_PATH"
+        LAUNCH="$(which mpirun) -n $NGPUS -N $NGPU_PER_HOST --hostfile $HOSTFILE -x PATH -x LD_LIBRARY_PATH"
+        # alias mpilaunch="${LAUNCH}"
+        # alias mpilaunch="$(which mpirun) -n $NGPUS -N $NGPU_PER_HOST --hostfile $HOSTFILE} -x PATH -x LD_LIBRARY_PATH"
     else
         echo "[setupThetaGPU]: Unexpected hostname $(hostname)"
     fi
@@ -79,18 +98,16 @@ function setupPolaris() {
         HOSTFILE=${HOSTFILE:-${PBS_NODEFILE}}
         CONDA_DATE="2023-10-04"
         VENV_DIR="${ROOT}/venvs/${MACHINE}/${CONDA_DATE}"
-        loadCondaEnv "${CONDA_DATE}"
-        setupVenv "${VENV_DIR}"
+        setupPython "${CONDA_DATE}" "${VENV_DIR}"
+        # loadCondaEnv "${CONDA_DATE}"
+        # setupVenv "${VENV_DIR}"
         # Distributed setup information
         NHOSTS=$(wc -l < "${HOSTFILE}")
         NGPU_PER_HOST=$(nvidia-smi -L | wc -l)
         NGPUS=$((NHOSTS * NGPU_PER_HOST))
-        echo "┌─────────────────────────────────────────────┐"
-        echo "│ Using ${NHOSTS} hosts from ${HOSTFILE}      │"
-        echo "│ Using ${NGPU_PER_HOST} GPUs per host        │"
-        echo "│ For a total of: ${NGPUS} GPUs               │"
-        echo "└─────────────────────────────────────────────┘"
-        alias mpilaunch="$(which mpiexec) --verbose --envall -n $NGPUS -ppn $NGPU_PER_HOST --hostfile ${HOSTFILE}"
+        LAUNCH="$(which mpiexec) --verbose --envall -n $NGPUS -ppn $NGPU_PER_HOST --hostfile ${HOSTFILE}"
+        # alias mpilaunch="$(which mpiexec) --verbose --envall -n $NGPUS -ppn $NGPU_PER_HOST --hostfile ${HOSTFILE}"
+        alias mpilaunch="${LAUNCH}"
     else
         echo "[setupPolaris]: Unexpected hostname $(hostname)"
     fi
@@ -111,7 +128,8 @@ function setupPerlmutter() {
         export NHOSTS="${SLURM_NNODES:-1}"
         export NGPU_PER_HOST="${SLURM_GPUS_ON_NODE:-$(nvidia-smi -L | wc -l)}"
         export NGPUS="$(( NHOSTS * NGPU_PER_HOST ))"
-        alias mpilaunch="$(which srun) -N ${NHOSTS} -n ${NGPUS} -l u"
+        LAUNCH="$(which srun) -N ${NHOSTS} -n ${NGPUS} -l u"
+        alias mpilaunch="${LAUNCH}"
     else
         echo "[setupPerlmutter]: Unexpected hostname $(hostname)"
     fi
@@ -125,12 +143,13 @@ function setupLogs() {
     if [ ! -d "${LOGDIR}" ]; then
         mkdir -p "${LOGDIR}"
     fi
+    echo "Writing to logfile: ${LOGFILE}"
     # Keep track of latest logfile for easy access
     echo "$LOGFILE" >> "${LOGDIR}/latest"
 }
 
 
-printJobInfo() {
+function printJobInfo() {
     ARGS=$*
     echo "┌─────────────────────────────────────────────────────────────────────┐"
     echo "│ [setup.sh]: Job started at: ${TSTAMP} on ${HOST} by ${USER}"
@@ -139,6 +158,8 @@ printJobInfo() {
     echo "┌─────────────────────────────────────────────────────────────────────┐"
     echo "│ [setup.sh]: DIR=${DIR}"
     echo "│ [setup.sh]: MAIN=${MAIN}"
+    echo "│ [setup.sh]: SETUP_SCRIPT=${SETUP_SCRIPT}"
+    echo "│ [setup.sh]: TRAIN_SCRIPT=${TRAIN_SCRIPT}"
     echo "│ [setup.sh]: PARENT=${PARENT}"
     echo "│ [setup.sh]: ROOT=${ROOT}"
     echo "│ [setup.sh]: LOGDIR=${LOGDIR}"
@@ -147,20 +168,18 @@ printJobInfo() {
     echo "┌─────────────────────────────────────────────────────────────────────┐"
     echo "│ [setup.sh]: Using ${NHOSTS} hosts from ${HOSTFILE}"
     echo "│ [setup.sh]: [Hosts]: "
-    echo "│ [setup.sh]:   ${HOSTS[*]}"
+    echo "│ [setup.sh]:   ${HOSTS[*]:-$(cat "$HOSTFILE"):-${SLURM_JOB_NODELIST}}"
     echo "│ [setup.sh]: With ${NGPU_PER_HOST} GPUs per host"
     echo "│ [setup.sh]: For a total of: ${NGPUS} GPUs"
     echo "└─────────────────────────────────────────────────────────────────────┘"
     echo "┌─────────────────────────────────────────────────────────────────────┐"
     echo "│ [setup.sh]: Using mpilaunch: $(which mpilaunch)"
     echo "│ [setup.sh]: Using python: $(which python3)"
-    echo "└─────────────────────────────────────────────────────────────────────┘"
-    echo "┌─────────────────────────────────────────────────────────────────────┐"
-    echo "│ [setup.sh]: MAIN: ${MAIN}"
-    echo "│ [setup.sh]: Writing logs to ${LOGFILE}"
     echo "│ [setup.sh]: ARGS: " && echo "${ARGS[*]}"
+    echo "│ [setup.sh]: LAUNCH: ${LAUNCH} $(which python3) ${MAIN} ${ARGS[*]}"
     echo "└─────────────────────────────────────────────────────────────────────┘"
     echo "┌─────────────────────────────────────────────────────────────────────┐"
+    echo "│ [setup.sh]: Writing logs to ${LOGFILE}"
     echo '│ [setup.sh]: To view output: `tail -f $(tail -1 logs/latest)`'  # noqa
     echo "│ [setup.sh]: Latest logfile: $(tail -1 ./logs/latest)"
     echo "│ [setup.sh]: tail -f $(tail -1 logs/latest)"
@@ -170,20 +189,27 @@ printJobInfo() {
 function setupJob() {
     if [[ $(hostname) == x3* ]]; then
         setupPolaris
+        HOSTS_ARR=$(cat "${HOSTFILE}")
+        HOSTS=$(join_by ' ' "${HOSTS_ARR}")
     elif [[ $(hostname) == thetagpu* ]]; then
         setupThetaGPU
+        HOSTS_ARR=$(cat "${HOSTFILE}")
+        HOSTS=$(join_by ' ' "${HOSTS_ARR}")
     elif [[ $(hostname) == nid* || $(hostname) == login* ]]; then
         setupPelmutter
+        HOSTS="${SLURM_JOB_NODELIST}"
     else
         echo "[setupJob]: Unexpected hostname $(hostname)"
-        alias mpirun=''
-        hostname > hostfile
-        HOSTFILE="hostfile"
+        # alias mpirun=''
+        # hostname > hostfile
+        # HOSTFILE="hostfile"
         # [[ "$(mpirun)" ] && alias mpilaunch='mpirun' || alias mpilaunch=''
         exit 1
     fi
-    HOSTS_ARR=$(cat "${HOSTFILE}")
-    HOSTS=$(join_by ' ' "${HOSTS_ARR}")
+    export NHOSTS
+    export NGPU_PER_HOST
+    export NGPUS
+    export HOSTFILE
+    export LAUNCH
     setupLogs
-    printJobInfo "$@"
 }
