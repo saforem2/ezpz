1eadc3d671685d3131e87ea27d4a80fe4d28b5e1
Sam Foreman <saforem2@gmail.com>
2023-10-17 09:00:32 -0500

    Move `train.py` to `__main__.py`

--- src/ezpz/__main__.py
+++ src/ezpz/__main__.py
@@ -1,22 +1,64 @@
+# -*- coding: utf-8 -*-
 """
 ezpz/__main__.py
 
+Contains main entry point for training.
 """
-from ezpz.dist import check
+from __future__ import (
+    absolute_import,
+    annotations,
+    division,
+    print_function,
+    unicode_literals
+)
+import os
+import sys
+
+import hydra
+import logging
+
+from rich.json import JSON
+from hydra.utils import instantiate
+from omegaconf.dictconfig import DictConfig
+
+from ezpz.dist import setup, setup_wandb
+from ezpz.configs import TrainConfig, git_ds_info
+
+log = logging.getLogger(__name__)
+
+
+@hydra.main(version_base=None, config_path='./conf', config_name='config')
+def main(cfg: DictConfig) -> int:
+    config = instantiate(cfg)
+    assert isinstance(config, TrainConfig)
+    rank = setup(
+        framework=config.framework,
+        backend=config.backend,
+        seed=config.seed
+    )
+    if rank != 0:
+        log.setLevel("CRITICAL")
+    else:
+        from rich import print_json
+        print_json(config.to_json())
+        if config.use_wandb:
+            setup_wandb(
+                project_name=config.wandb_project_name,
+                config=cfg,
+            )
+    # log.info(f'config: {JSON(config.to_json())}')
+    # log.info(json.dumps())
+    # log.info(f'config: {config.}')
+    log.info(f'Output dir: {os.getcwd()}')
+    if rank == 0 and config.backend.lower() in ['ds', 'dspeed', 'deepspeed']:
+        git_ds_info()
+    return rank
 
 
 if __name__ == '__main__':
-    import sys
-    try:
-        framework = sys.argv[1]
-    except IndexError:
-        framework = 'pytorch'
-    try:
-        backend = sys.argv[2]
-    except IndexError:
-        backend = 'deepspeed'
-    try:
-        port = sys.argv[3]
-    except IndexError:
-        port = '5432'
-    check(framework=framework, backend=backend, port=port)
+    # import wandb
+    # wandb.require(experiment='service')
+    rank = main()
+    # if wandb.run is not None:
+    #     wandb.finish(0)
+    sys.exit(0)
