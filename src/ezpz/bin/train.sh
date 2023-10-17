e4bee015b9402d45a299f22b4d396df65d09cfcd
Sam Foreman <saforem2@gmail.com>
2023-10-16 23:14:36 -0500

    Update `bin/train.sh`

--- src/ezpz/bin/train.sh
+++ src/ezpz/bin/train.sh
@@ -7,48 +7,65 @@
 # cd $PBS_O_WORKDIR
 # ----------------------------------------------------------------------------
 #
-resolveDir() {
-  SOURCE=${BASH_SOURCE[0]}
-  while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+function resolveDir1() {
+    SOURCE=${BASH_SOURCE[0]}
+    while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+        SOURCE=$(readlink "$SOURCE")
+        [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
     DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-    SOURCE=$(readlink "$SOURCE")
-    [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-  done
-  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-  HERE="${DIR}/train.sh"
-  PARENT=$(dirname "${DIR}")
-  MAIN="${PARENT}/main.py"
-  GRANDPARENT=$(dirname "${PARENT}")
-  ROOT=$(dirname "${GRANDPARENT}")
-  echo "DIR: ${DIR}"
-  echo "HERE: ${HERE}"
-  echo "PARENT: ${PARENT}"
-  echo "GRANDPARENT: ${GRANDPARENT}"
-  echo "ROOT: ${ROOT}"
+    HERE="${DIR}/train.sh"
+    PARENT=$(dirname "${DIR}")
+    MAIN="${PARENT}/train.py"
+    GRANDPARENT=$(dirname "${PARENT}")
+    ROOT=$(dirname "${GRANDPARENT}")
+    # echo "DIR: ${DIR}"
+    # echo "HERE: ${HERE}"
+    # echo "PARENT: ${PARENT}"
+    # echo "GRANDPARENT: ${GRANDPARENT}"
+    # echo "ROOT: ${ROOT}"
+}
+
+
+function resolveDir() {
+    # Resolve path to current file
+    SOURCE=${BASH_SOURCE[0]}
+    while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+        SOURCE=$(readlink "$SOURCE")
+        [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+    PARENT=$(dirname "${DIR}")
+    MAIN="$DIR/train.py"
+    SETUP_SCRIPT="$DIR/bin/setup.sh"
+    TRAIN_SCRIPT="$DIR/bin/train.sh"
+    ROOT=$(dirname "${PARENT}")
 }
 
 
 function sourceFile() {
-  fpath="${1:-${DIR}/setup.sh}"
-  # SETUP_FILE="${DIR}/setup.sh"
-  if [[ -f "${fpath}" ]]; then
-    echo "source-ing ${fpath}"
-    # shellcheck source=./setup.sh
-    source "${fpath}"
-  else
-    echo "ERROR: UNABLE TO SOURCE ${fpath}"
-  fi
+    fpath="${1:-${DIR}/setup.sh}"
+    # SETUP_FILE="${DIR}/setup.sh"
+    if [[ -f "${fpath}" ]]; then
+        echo "source-ing ${fpath}"
+        # shellcheck source=./setup.sh
+        source "${fpath}"
+    else
+        echo "ERROR: UNABLE TO SOURCE ${fpath}"
+    fi
 }
 
 #┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 #┃ Make sure we're not already running; if so, exit here ┃
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 function killIfRunning() {
-  PIDS=$(ps aux | grep -E 'mpi.+main.+py' | grep -v grep | awk '{print $2}')
-  if [ -n "${PIDS}" ]; then
-    echo "Already running! Exiting!"
-    exit 1
-  fi
+    PIDS=$(ps aux | grep -E 'mpi.+train.+py' | grep -v grep | awk '{print $2}')
+    if [ -n "${PIDS}" ]; then
+        echo "Already running! Exiting!"
+        exit 1
+    fi
 }
 
 
@@ -56,20 +73,24 @@ function killIfRunning() {
 # ┃ SETUP CONDA + MPI ENVIRONMENT @ ALCF ┃
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 function setup() {
-  resolveDir
-  killIfRunning
-  # setupEnv
-  # setupLogs
-  sourceFile "${DIR}/setup.sh"
-  setupJob "$@" | tee -a "${LOGFILE}"
-  export NODE_RANK=0
-  export NNODES=$NHOSTS
-  export GPUS_PER_NODE=$NGPU_PER_HOST
-  export WORLD_SIZE=$NGPUS
-  # export LC_ALL=$(locale -a | grep UTF-8)
-  # printJobInfo "$@" | tee -a "${LOGFILE}"
+    local args="$*"
+    resolveDir
+    killIfRunning
+    sourceFile "${DIR}/setup.sh"
+    setupJob
+    EXEC="${LAUNCH} $(which python3) ${MAIN} ${args}"
+    export EXEC
+    echo "EXEC: ${EXEC}"
+    printJobInfo "$@" | tee -a "${LOGFILE}"
+    export NODE_RANK=0
+    export NNODES=$NHOSTS
+    export GPUS_PER_NODE=$NGPU_PER_HOST
+    export WORLD_SIZE=$NGPUS
 }
 
 setup "$@"
-mpilaunch "$(which python3)" "${MAIN}" "$@" > "${LOGFILE}" 2>&1 &
+${EXEC} | tee -a "${LOGFILE}"
+wait $!
+# "${EXEC}" > "${LOGFILE}" 2>&1
+# "${EXEC}" "$(which python3)" "${MAIN}" "$@" > "${LOGFILE}" 2>&1 &
 # wait $!
