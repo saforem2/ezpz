8038a020e25ab951ac2edbac37424a02fa100fce
Sam Foreman <saforem2@gmail.com>
2023-11-01 13:05:18 -0500

    Update `__init__.py`

--- src/ezpz/__init__.py
+++ src/ezpz/__init__.py
@@ -4,18 +4,21 @@ ezpz/__init__.py
 from __future__ import absolute_import, annotations, division, print_function
 import logging
 import os
-from typing import Optional
-from enrich.console import is_interactive, get_console
+# from typing import Optional
+# from enrich import get_logger
+# from enrich.console import is_interactive, get_console
 # import warnings
 
 from mpi4py import MPI
-from enrich.handler import RichHandler
-import tqdm
+
+# from enrich.handler import RichHandler
 # from rich import print
 # from enrich import get_logger
 # from pathlib import Path
-from ezpz import dist
-from ezpz.dist import (
+# pyright: ignore 
+# noqa:E402
+from ezpz import dist    # noqa:E402
+from ezpz.dist import (  # noqa:E402
     setup_wandb,
     seed_everything,
     setup_tensorflow,
@@ -27,13 +30,17 @@ from ezpz.dist import (
     query_environment,
     check
 )
+# from ezpz.logging import get_logger, get_rich_logger, get_file_logger, get_console
 
-from ezpz.configs import (
+from ezpz.configs import (  # noqa:E402
     HERE,
     PROJECT_DIR,
     PROJECT_ROOT,
     CONF_DIR,
     LOGS_DIR,
+    BIN_DIR,
+    SAVEJOBENV,
+    GETJOBENV,
     OUTPUTS_DIR,
     QUARTO_OUTPUTS_DIR,
     FRAMEWORKS,
@@ -49,6 +56,10 @@ __all__ = [
     'setup_torch',
     'cleanup',
     'seed_everything',
+    # 'get_console',
+    # 'get_logger',
+    # 'get_rich_logger',
+    # 'get_file_logger',
     'get_rank',
     'get_local_rank',
     'get_world_size',
@@ -63,6 +74,9 @@ __all__ = [
     'QUARTO_OUTPUTS_DIR',
     'FRAMEWORKS',
     'BACKENDS',
+    'BIN_DIR',
+    'GETJOBENV',
+    'SAVEJOBENV',
     'load_ds_config',
     'TrainConfig',
 ]
@@ -71,6 +85,13 @@ __all__ = [
 os.environ['PYTHONIOENCODING'] = 'utf-8'
 RANK = int(MPI.COMM_WORLD.Get_rank())
 WORLD_SIZE = int(MPI.COMM_WORLD.Get_size())
+log = logging.getLogger(__name__)
+# LEVEL = "INFO" if RANK == 0 else "CRITICAL"
+# log = get_logger(__name__, "INFO")
+# if RANK != 0:
+#     log.setLevel("CRITICAL")
+
+
 
 # # Check that MPS is available
 # if (
@@ -90,145 +111,8 @@ WORLD_SIZE = int(MPI.COMM_WORLD.Get_size())
 # DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 # print(f"Using device: {DEVICE}")
 
+def savejobenv():
+    return SAVEJOBENV
 
-class DummyTqdmFile(object):
-    """ Dummy file-like that will write to tqdm
-    https://github.com/tqdm/tqdm/issues/313
-    """
-    file = None
-
-    def __init__(self, file):
-        self.file = file
-
-    def write(self, x):
-        # Avoid print() second call (useless \n)
-        # if len(x.rstrip()) > 0:
-        tqdm.tqdm.write(x, file=self.file, end='\n')
-
-    def flush(self):
-        return getattr(self.file, "flush", lambda: None)()
-
-
-def get_rich_logger(
-        name: Optional[str] = None,
-        level: str = 'INFO'
-) -> logging.Logger:
-    # log: logging.Logger = get_logger(name=name, level=level)
-    log = logging.getLogger(name)
-    log.handlers = []
-    console = get_console(
-        markup=True,
-        redirect=(WORLD_SIZE > 1),
-    )
-    handler = RichHandler(
-        level,
-        rich_tracebacks=False,
-        console=console,
-        show_path=False,
-        enable_link_path=False
-    )
-    log.handlers = [handler]
-    log.setLevel(level)
-    return log
-
-
-def get_file_logger(
-        name: Optional[str] = None,
-        level: str = 'INFO',
-        rank_zero_only: bool = True,
-        fname: Optional[str] = None,
-        # rich_stdout: bool = True,
-) -> logging.Logger:
-    # logging.basicConfig(stream=DummyTqdmFile(sys.stderr))
-    import logging
-    fname = 'ezpz' if fname is None else fname
-    log = logging.getLogger(name)
-    if rank_zero_only:
-        fh = logging.FileHandler(f"{fname}.log")
-        if RANK == 0:
-            log.setLevel(level)
-            fh.setLevel(level)
-        else:
-            log.setLevel('CRITICAL')
-            fh.setLevel('CRITICAL')
-    else:
-        fh = logging.FileHandler(f"{fname}-{RANK}.log")
-        log.setLevel(level)
-        fh.setLevel(level)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter(
-        "[%(asctime)s][%(name)s][%(levelname)s] - %(message)s"
-    )
-    fh.setFormatter(formatter)
-    log.addHandler(fh)
-    return log
-
-
-def get_logger(
-        name: Optional[str] = None,
-        level: str = 'INFO',
-        rank_zero_only: bool = True,
-        **kwargs,
-) -> logging.Logger:
-    log = logging.getLogger(name)
-    # log.handlers = []
-    # from rich.logging import RichHandler
-    # from l2hmc.utils.rich import get_console, is_interactive
-    from enrich.console import get_console
-    # from enrich import is_interactive
-    # format = "[%(asctime)s][%(name)s][%(levelname)s] - %(message)s"
-    if rank_zero_only:
-        if RANK != 0:
-            log.setLevel('CRITICAL')
-        else:
-            log.setLevel(level)
-    if RANK == 0:
-        console = get_console(
-            markup=True,  # (WORLD_SIZE == 1),
-            redirect=(WORLD_SIZE > 1),
-            **kwargs
-        )
-        if console.is_jupyter:
-            console.is_jupyter = False
-        # log.propagate = True
-        # log.handlers = []
-        use_markup = (
-            WORLD_SIZE == 1
-            and not is_interactive()
-        )
-        log.addHandler(
-            RichHandler(
-                omit_repeated_times=False,
-                level=level,
-                console=console,
-                show_time=True,
-                show_level=True,
-                show_path=True,
-                markup=use_markup,
-                enable_link_path=use_markup,
-            )
-        )
-        log.setLevel(level)
-    if (
-            len(log.handlers) > 1
-            and all([i == log.handlers[0] for i in log.handlers])
-    ):
-        log.handlers = [log.handlers[0]]
-    return log
-
-#
-# if __name__ == '__main__':
-#     import sys
-#     try:
-#         framework = sys.argv[1]
-#     except IndexError:
-#         framework = 'pytorch'
-#     try:
-#         backend = sys.argv[2]
-#     except IndexError:
-#         backend = 'deepspeed'
-#     try:
-#         port = sys.argv[3]
-#     except IndexError:
-#         port = '5432'
-#     check(framework=framework, backend=backend, port=port)
+def get_jobenv():
+    return GETJOBENV
