2381a676623a56607db6299caf2daa4e8bc1433c
Sam Foreman <saforem2@gmail.com>
2024-02-06 09:46:42 -0600

    Update `__init__.py`

--- src/ezpz/__init__.py
+++ src/ezpz/__init__.py
@@ -3,153 +3,217 @@ ezpz/__init__.py
 """
 from __future__ import absolute_import, annotations, division, print_function
 import logging
+import logging.config
 import os
+import re
 from typing import Any, Optional
 from typing import Union
 
 from enrich.console import get_console, is_interactive
+from mpi4py import MPI
+import numpy as np
 from rich.console import Console
 from rich.logging import RichHandler
+import torch
+import tqdm
+
+from ezpz import dist
 from ezpz.configs import (
-    # getjobenv,
-    # savejobenv,
-    command_exists,
-    git_ds_info,
-    load_ds_config,
     BACKENDS,
-    get_scheduler,
-    print_config_tree,
     BIN_DIR,
     CONF_DIR,
     FRAMEWORKS,
+    GETJOBENV,
     HERE,
     LOGS_DIR,
     OUTPUTS_DIR,
     PROJECT_DIR,
     PROJECT_ROOT,
     QUARTO_OUTPUTS_DIR,
-    SCHEDULERS,
     SAVEJOBENV,
-    GETJOBENV,
+    SCHEDULERS,
     TrainConfig,
+    command_exists,
+    get_logging_config,
+    get_scheduler,
+    git_ds_info,
+    load_ds_config,
+    print_config_tree,
 )
-
-from mpi4py import MPI
-import numpy as np
-import torch
-import tqdm
+from ezpz.dist import (
+    build_mpiexec_thetagpu,
+    check,
+    cleanup,
+    get_cobalt_nodefile,
+    get_cobalt_resources,
+    get_cpus_per_node,
+    get_dist_info,
+    get_gpus_per_node,
+    get_hostname,
+    get_hosts_from_hostfile,
+    get_local_rank,
+    get_machine,
+    get_node_index,
+    get_nodes_from_hostfile,
+    get_num_nodes,
+    get_rank,
+    get_torch_backend,
+    get_torch_device,
+    get_world_size,
+    include_file,
+    init_deepspeed,
+    init_process_group,
+    inspect_cobalt_running_job,
+    print_dist_setup,
+    query_environment,
+    run_bash_command,
+    seed_everything,
+    setup,
+    setup_tensorflow,
+    setup_torch,
+    setup_torch_DDP,
+    setup_torch_distributed,
+    setup_wandb,
+    timeit,
+    timeitlogit,
+)
+from ezpz.jobs import loadjobenv, savejobenv
 try:
     import wandb  # type:ignore noqa
 except Exception:
     wandb = None
 
-from ezpz import dist
-from ezpz.dist import (
-    build_mpiexec_thetagpu,
-    check,
-    timeit,
-    timeitlogit,
-    setup,
-    cleanup,
-    get_dist_info,
-    get_torch_device,
-    get_torch_backend,
-    get_hosts_from_hostfile,
-    get_machine,
-    get_hostname,
-    get_local_rank,
-    get_rank,
-    get_cobalt_nodefile,
-    get_nodes_from_hostfile,
-    get_num_nodes,
-    get_gpus_per_node,
-    get_cpus_per_node,
-    get_node_index,
-    get_cobalt_resources,
-    get_world_size,
-    init_process_group,
-    init_deepspeed,     # ✔︎
-    include_file,
-    query_environment,
-    print_dist_setup,    # ✔︎
-    run_bash_command,
-    setup_torch_DDP,    # ✔︎
-    seed_everything,     # ✔︎
-    setup_tensorflow,    # ✔︎
-    setup_torch,         # ✔︎
-    setup_torch_distributed,  # ✔︎
-    setup_wandb,         # ✔︎
-    inspect_cobalt_running_job,
-)
 
-from ezpz.jobs import (
-    savejobenv,
-    loadjobenv,
-)
-
+log_config = logging.config.dictConfig(get_logging_config())
 log = logging.getLogger(__name__)
+log.setLevel('INFO')
+logging.getLogger('sh').setLevel('WARNING')
 
 __all__ = [
-    'build_mpiexec_thetagpu',
-    'dist',
-    'setup',
-    'setup_wandb',
-    'print_config_tree',
-    'setup_tensorflow',
-    'loadjobenv',
-    'savejobenv',
-    'command_exists',
-    'print_dist_setup',
-    'get_torch_device',
-    'get_scheduler',
-    'get_hostname',
-    'get_hosts_from_hostfile',
-    'get_torch_backend',
-    'get_scheduler',
-    'get_cpus_per_node',
-    'get_num_nodes',
-    'git_ds_info',
-    'init_process_group',
-    'get_dist_info',
-    'get_machine',
-    'setup_torch',
-    'timeit',
-    'timeitlogit',
-    'setup_torch_DDP',
-    'include_file',
-    'init_deepspeed',
-    'setup_torch_distributed',
-    'inspect_cobalt_running_job',
-    'get_cobalt_nodefile',
-    'get_cobalt_resources',
-    'get_nodes_from_hostfile',
-    'get_gpus_per_node',
-    'cleanup',
-    'seed_everything',
-    'run_bash_command',
-    'get_rank',
-    'get_local_rank',
-    'get_node_index',
-    'get_world_size',
-    'query_environment',
-    'check',
-    'HERE',
+    'BACKENDS',
+    'BIN_DIR',
+    'CONF_DIR',
+    'FRAMEWORKS',
+    'GETJOBENV', 'HERE',
+    'LOGS_DIR',
+    'OUTPUTS_DIR',
+    'PROJECT_DIR',
     'PROJECT_DIR',
     'PROJECT_ROOT',
-    'CONF_DIR',
-    'LOGS_DIR',
-    'SCHEDULERS',
-    'BIN_DIR',
-    "SAVEJOBENV",
-    "GETJOBENV",
-    'OUTPUTS_DIR',
     'QUARTO_OUTPUTS_DIR',
-    'FRAMEWORKS',
-    'BACKENDS',
-    'load_ds_config',
+    'SAVEJOBENV',
+    'SCHEDULERS',
     'TrainConfig',
+    'build_mpiexec_thetagpu',
+    'check',
+    'cleanup',
+    'command_exists',
+    'dist',
+    'get_cobalt_nodefile',
+    'get_cobalt_resources',
+    'get_cpus_per_node',
+    'get_dist_info',
+    'get_gpus_per_node',
+    'get_hostname',
+    'get_hosts_from_hostfile',
+    'get_local_rank',
+    'get_logging_config',
+    'get_machine',
+    'get_node_index',
+    'get_nodes_from_hostfile',
+    'get_num_nodes',
+    'get_rank',
+    'get_scheduler',
+    'get_scheduler',
+    'get_torch_backend',
+    'get_torch_device',
+    'get_world_size',
+    'git_ds_info',
     'grab_tensor',
+    'include_file',
+    'init_deepspeed',
+    'init_process_group',
+    'inspect_cobalt_running_job',
+    'load_ds_config',
+    'loadjobenv',
+    'print_config_tree',
+    'print_dist_setup',
+    'query_environment',
+    'run_bash_command',
+    'savejobenv',
+    'seed_everything',
+    'setup',
+    'setup_tensorflow',
+    'setup_torch',
+    'setup_torch_DDP',
+    'setup_torch_distributed',
+    'setup_wandb',
+    'timeit',
+    'timeitlogit'
 ]
+
+# __all__ = [
+#     'build_mpiexec_thetagpu',
+#     'dist',
+#     'loadjobenv',
+#     'command_exists',
+#     'get_loggingg_config',
+#     'get_torch_device',
+#     'get_scheduler',
+#     'get_hostname',
+#     'get_hosts_from_hostfile',
+#     'get_torch_backend',
+#     'get_scheduler',
+#     'get_cpus_per_node',
+#     'get_num_nodes',
+#     'git_ds_info',
+#     'init_process_group',
+#     'get_dist_info',
+#     'get_machine',
+#     'setup_torch',
+#     'timeit',
+#     'timeitlogit',
+#     'setup_torch_DDP',
+#     'include_file',
+#     'init_deepspeed',
+#     'inspect_cobalt_running_job',
+#     'get_cobalt_nodefile',
+#     'get_cobalt_resources',
+#     'get_nodes_from_hostfile',
+#     'get_gpus_per_node',
+#     'cleanup',
+#     'seed_everything',
+#     'run_bash_command',
+#     'get_rank',
+#     'get_local_rank',
+#     'get_node_index',
+#     'get_world_size',
+#     'query_environment',
+#     'check',
+#     'HERE',
+#     'setup_torch_distributed',
+#     'setup',
+#     'setup_wandb',
+#     'print_config_tree',
+#     'setup_tensorflow',
+#     'savejobenv',
+#     'print_dist_setup',
+#     'PROJECT_DIR',
+#     'PROJECT_ROOT',
+#     'CONF_DIR',
+#     'LOGS_DIR',
+#     'SCHEDULERS',
+#     'BIN_DIR',
+#     "SAVEJOBENV",
+#     "GETJOBENV",
+#     'OUTPUTS_DIR',
+#     'QUARTO_OUTPUTS_DIR',
+#     'FRAMEWORKS',
+#     'BACKENDS',
+#     'load_ds_config',
+#     'TrainConfig',
+#     'grab_tensor',
+# ]
 
 
 os.environ['PYTHONIOENCODING'] = 'utf-8'
@@ -157,6 +221,10 @@ RANK = int(MPI.COMM_WORLD.Get_rank())
 WORLD_SIZE = int(MPI.COMM_WORLD.Get_size())
 
 ScalarLike = Union[int, float, bool, np.floating]
+
+
+def normalize(name):
+    return re.sub(r"[-_.]+", "-", name).lower()
 
 
 def get_console_from_logger(logger: logging.Logger) -> Console:
@@ -179,7 +247,10 @@ def grab_tensor(x: Any) -> np.ndarray | ScalarLike | None:
         elif isinstance(x[0], np.ndarray):
             return np.stack(x)
         else:
-            import tensorflow as tf
+            try:
+                import tensorflow as tf  # type:ignore
+            except (ImportError, ModuleNotFoundError) as exc:
+                raise exc
             if isinstance(x[0], tf.Tensor):
                 return grab_tensor(tf.stack(x))
     elif isinstance(x, np.ndarray):
