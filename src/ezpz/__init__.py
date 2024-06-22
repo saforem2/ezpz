4adf743e2715075f64685eedaa2726b53e75b66e
Sam Foreman <saforem2@gmail.com>
2024-06-21 19:01:50 -0500

    [format] `ezpz/__init__.py`

--- src/ezpz/__init__.py
+++ src/ezpz/__init__.py
@@ -1,14 +1,14 @@
 """
 ezpz/__init__.py
 """
+
 from __future__ import absolute_import, annotations, division, print_function
-# import socket
-# if socket.gethostname().startswith('x3'):
 from mpi4py import MPI
 import logging
 import logging.config
 import os
 import re
+
 # import socket
 from typing import Any, Optional
 from typing import Union
@@ -104,19 +104,11 @@ from ezpz.log.style import (
 from ezpz.profile import PyInstrumentProfiler, get_context_manager
 
 try:
-    import wandb  # type: ignore
+    import wandb  # pyright: ignore
 except Exception:
     wandb = None
 
 
-# import tqdm
-# from mpi4py import MPI
-
-
-# import importlib.util
-# import sys
-
-#
 # def lazy_import(name: str):
 #     spec = importlib.util.find_spec(name)
 #     loader = importlib.util.LazyLoader(spec.loader)
@@ -130,126 +122,150 @@ except Exception:
 log_config = logging.config.dictConfig(get_logging_config())
 log = logging.getLogger(__name__)
 # log.setLevel('INFO')
-logging.getLogger('sh').setLevel('WARNING')
+logging.getLogger("sh").setLevel("WARNING")
 
-os.environ['PYTHONIOENCODING'] = 'utf-8'
+
+ScalarLike = Union[int, float, bool, np.floating]
+os.environ["PYTHONIOENCODING"] = "utf-8"
 # noqa: E402
 RANK = int(MPI.COMM_WORLD.Get_rank())
 WORLD_SIZE = int(MPI.COMM_WORLD.Get_size())
 
 log.setLevel("INFO") if RANK == 0 else log.setLevel("CRITICAL")
+log.info("Setting logging level to 'INFO' on 'RANK == 0'")
+log.info("Setting logging level to 'CRITICAL' on 'RANK != 0'")
+log.info(
+    ' ' .join(
+        [
+            "To disable this behavior,",
+            "and log from ALL ranks (not recommended), set:",
+            "'export LOG_FROM_ALL_RANKS=1' in your environment, and re-run."
+        ]
+    )
+)
+LOG_FROM_ALL_RANKS = os.environ.get(
+    "LOG_FROM_ALL_RANKS",
+    os.environ.get(
+        "LOG_FROM_ALL_RANK",
+        False
+    )
+)
+if LOG_FROM_ALL_RANKS:
+    log.setLevel("INFO")
+else:
+    log.setLevel("INFO") if RANK == 0 else log.setLevel("CRITICAL")
 
-ScalarLike = Union[int, float, bool, np.floating]
 
 __all__ = [
-    'BACKENDS',
-    'BEAT_TIME',
-    'BIN_DIR',
-    'COLORS',
-    'CONF_DIR',
-    'Console',
-    'CustomLogging',
-    'DEFAULT_STYLES',
-    'FRAMEWORKS',
-    'FluidLogRender',
-    'GETJOBENV',
-    'HERE',
-    'LOGS_DIR',
-    'NO_COLOR',
-    'OUTPUTS_DIR',
-    'PROJECT_DIR',
-    'PROJECT_DIR',
-    'PROJECT_ROOT',
-    'QUARTO_OUTPUTS_DIR',
-    'RichHandler',
-    'SAVEJOBENV',
-    'SCHEDULERS',
-    'STYLES',
-    'PyInstrumentProfiler',
-    'TrainConfig',
-    'add_columns',
-    'build_layout',
-    'check',
-    'cleanup',
-    'command_exists',
-    'dist',
-    'flatten_dict',
-    'get_console',
-    'get_context_manager',
-    'get_cpus_per_node',
-    'get_dist_info',
-    'get_file_logger',
-    'get_gpus_per_node',
-    'get_hostname',
-    'get_hosts_from_hostfile',
-    'get_local_rank',
-    'get_logger',
-    'get_logging_config',
-    'get_machine',
-    'get_node_index',
-    'get_nodes_from_hostfile',
-    'get_num_nodes',
-    'get_rank',
-    'get_scheduler',
-    'get_scheduler',
-    'get_theme',
-    'get_torch_backend',
-    'get_torch_device',
-    'get_width',
-    'get_world_size',
-    'git_ds_info',
-    'grab_tensor',
-    'include_file',
-    'init_deepspeed',
-    'init_process_group',
-    'is_interactive',
-    'load_ds_config',
-    'loadjobenv',
-    'make_layout',
-    'nested_dict_to_df',
-    'plot',
-    'print_config',
-    'print_config_tree',
-    'print_dist_setup',
-    'printarr',
-    'query_environment',
-    'run_bash_command',
-    'savejobenv',
-    'seed_everything',
-    'setup',
-    'setup_tensorflow',
-    'setup_torch',
-    'setup_torch_DDP',
-    'setup_torch_distributed',
-    'setup_wandb',
-    'should_do_markup',
-    'timeit',
-    'timeitlogit',
-    'to_bool',
+    "BACKENDS",
+    "BEAT_TIME",
+    "BIN_DIR",
+    "COLORS",
+    "CONF_DIR",
+    "Console",
+    "CustomLogging",
+    "DEFAULT_STYLES",
+    "FRAMEWORKS",
+    "FluidLogRender",
+    "GETJOBENV",
+    "HERE",
+    "LOGS_DIR",
+    "NO_COLOR",
+    "OUTPUTS_DIR",
+    "PROJECT_DIR",
+    "PROJECT_DIR",
+    "PROJECT_ROOT",
+    "QUARTO_OUTPUTS_DIR",
+    "RichHandler",
+    "SAVEJOBENV",
+    "SCHEDULERS",
+    "STYLES",
+    "PyInstrumentProfiler",
+    "TrainConfig",
+    "add_columns",
+    "build_layout",
+    "check",
+    "cleanup",
+    "command_exists",
+    "dist",
+    "flatten_dict",
+    "get_console",
+    "get_context_manager",
+    "get_cpus_per_node",
+    "get_dist_info",
+    "get_file_logger",
+    "get_gpus_per_node",
+    "get_hostname",
+    "get_hosts_from_hostfile",
+    "get_local_rank",
+    "get_logger",
+    "get_logging_config",
+    "get_machine",
+    "get_node_index",
+    "get_nodes_from_hostfile",
+    "get_num_nodes",
+    "get_rank",
+    "get_scheduler",
+    "get_scheduler",
+    "get_theme",
+    "get_torch_backend",
+    "get_torch_device",
+    "get_width",
+    "get_world_size",
+    "git_ds_info",
+    "grab_tensor",
+    "include_file",
+    "init_deepspeed",
+    "init_process_group",
+    "is_interactive",
+    "load_ds_config",
+    "loadjobenv",
+    "make_layout",
+    "nested_dict_to_df",
+    "plot",
+    "print_config",
+    "print_config_tree",
+    "print_dist_setup",
+    "printarr",
+    "query_environment",
+    "run_bash_command",
+    "savejobenv",
+    "seed_everything",
+    "setup",
+    "setup_tensorflow",
+    "setup_torch",
+    "setup_torch_DDP",
+    "setup_torch_distributed",
+    "setup_wandb",
+    "should_do_markup",
+    "timeit",
+    "timeitlogit",
+    "to_bool",
 ]
 
-# assert __all__ == sorted
 
-
-def get_timestamp(fstr=None) -> str:
+def get_timestamp(fstr: Optional[str] = None) -> str:
     """Get formatted timestamp."""
     import datetime
+
     now = datetime.datetime.now()
     if fstr is None:
-        return now.strftime('%Y-%m-%d-%H%M%S')
+        return now.strftime("%Y-%m-%d-%H%M%S")
     return now.strftime(fstr)
 
 
-def normalize(name):
+def normalize(name: str) -> str:
     return re.sub(r"[-_.]+", "-", name).lower()
 
 
 def get_console_from_logger(logger: logging.Logger) -> Console:
     from ezpz.log.handler import RichHandler as EnrichHandler
+
     for handler in logger.handlers:
         if isinstance(handler, (RichHandler, EnrichHandler)):
             return handler.console
     from ezpz.log.console import get_console
+
     return get_console()
 
 
@@ -274,17 +290,15 @@ def grab_tensor(x: Any) -> np.ndarray | ScalarLike | None:
         return x
     elif isinstance(x, torch.Tensor):
         return x.detach().cpu().numpy()
-    elif callable(getattr(x, 'numpy', None)):
-        assert callable(getattr(x, 'numpy'))
+    elif callable(getattr(x, "numpy", None)):
+        assert callable(getattr(x, "numpy"))
         return x.numpy()
     raise ValueError
 
 
-def get_rich_logger(
-        name: Optional[str] = None,
-        level: str = 'INFO'
-) -> logging.Logger:
+def get_rich_logger(name: Optional[str] = None, level: str = "INFO") -> logging.Logger:
     from ezpz.log.handler import RichHandler
+
     # log: logging.Logger = get_logger(name=name, level=level)
     log = logging.getLogger(name)
     log.handlers = []
@@ -297,7 +311,7 @@ def get_rich_logger(
         rich_tracebacks=False,
         console=console,
         show_path=False,
-        enable_link_path=False
+        enable_link_path=False,
     )
     log.handlers = [handler]
     log.setLevel(level)
@@ -325,10 +339,8 @@ def get_rich_logger(
 #     return log
 
 
-def get_enrich_logging_config_as_yaml(
-        name: str = 'enrich',
-        level: str = 'INFO') -> str:
-    return fr"""
+def get_enrich_logging_config_as_yaml(name: str = "enrich", level: str = "INFO") -> str:
+    return rf"""
     ---
     # version: 1
     handlers:
@@ -346,17 +358,13 @@ def get_enrich_logging_config_as_yaml(
 
 
 def get_logger_new(
-        name: str,
-        level: str = 'INFO',
+    name: str,
+    level: str = "INFO",
 ):
     import yaml
     config = yaml.safe_load(
-        get_enrich_logging_config_as_yaml(
-            name=name,
-            level=level
-        ),
+        get_enrich_logging_config_as_yaml(name=name, level=level),
     )
-    #     #     config = yaml.load(stream, Loader=yaml.FullLoader)
     logging.config.dictConfig(config)
     log = logging.getLogger(name=name)
     log.setLevel(level)
