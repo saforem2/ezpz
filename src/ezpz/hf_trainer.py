d1edbdab4f07dc85c1a030f40a0ddf0339f8cc68
Sam Foreman <saforem2@gmail.com>
2025-03-28 11:10:03 -0500

    feat: Update `ezpz/hf_trainer.py`

--- src/ezpz/hf_trainer.py
+++ src/ezpz/hf_trainer.py
@@ -252,17 +252,16 @@ class DataTrainingArguments:
             raise ValueError(
                 "Need either a dataset name or a training/validation file."
             )
-        else:
-            if self.train_file is not None:
-                extension = self.train_file.split(".")[-1]
-                assert extension in ["csv", "json", "txt"], (
-                    "`train_file` should be a csv, a json or a txt file."
-                )
-            if self.validation_file is not None:
-                extension = self.validation_file.split(".")[-1]
-                assert extension in ["csv", "json", "txt"], (
-                    "`validation_file` should be a csv, a json or a txt file."
-                )
+        if self.train_file is not None:
+            extension = self.train_file.split(".")[-1]
+            assert extension in ["csv", "json", "txt"], (
+                "`train_file` should be a csv, a json or a txt file."
+            )
+        if self.validation_file is not None:
+            extension = self.validation_file.split(".")[-1]
+            assert extension in ["csv", "json", "txt"], (
+                "`validation_file` should be a csv, a json or a txt file."
+            )
 
 
 def main():
