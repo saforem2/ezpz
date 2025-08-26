ce1af02788bfdfcbe0e9526021562a19fa9fe3bf
Sam Foreman <saforem2@gmail.com>
2025-08-26 09:59:44 -0500

    chore: Update `src/ezpz/pbs.py`

--- src/ezpz/pbs.py
+++ src/ezpz/pbs.py
@@ -144,57 +144,94 @@ def get_pbs_launch_cmd(
     else:
         nhosts = nhosts_max
 
-    if ngpus is None and ngpu_per_host is not None:
-        ngpus = ngpu_per_host * ezpz.get_num_nodes(hostfile=Path(hostfile))
-    elif ngpus is None and ngpu_per_host is None:
-        ngpus = ngpus_max
-        ngpu_per_host = ngpu_per_host_max
-    elif ngpus is not None:
+    #  ---- [`ngpus`, `nhosts`, `ngpu_per_host` logic] -------------------------
+    #  ```bash
+    # 1. [ ❌, ✅, ❌ ] -> [ ngpus_max, nhosts_max, ngpu_per_host_max ]
+    # 2. [ ❌, ✅, ✅ ] -> [ ngpu_per_host * nhosts_max, nhosts_max, ngpu_per_host ]
+    # 3. [ ❌, ✅, ❌ ] -> [ ngpus_max, nhosts, ngpus_max // nhosts ]
+    # 4. [ ✅, ✅, ❌ ] -> [ ngpus, ngpus // ngpu_per_host_max, ngpu_per_host_max ]
+    # 5. [ ❌, ✅, ✅ ] -> [ ngpu_per_host * nhosts, nhosts, ngpu_per_host ]
+    # 6. [ ✅, ✅, ✅ ] -> [ ngpus, ngpus // ngpu_per_host, ngpu_per_host ]
+    # 7. [ ✅, ✅, ❌ ] -> [ ngpus, nhosts, ngpus // nhosts ]
+    # 8. [ ✅, ✅, ✅ ] -> [ ngpus, nhosts, ngpu_per_host ]
+    # ```
+    #
+    if ngpus is None:
         if ngpu_per_host is None and nhosts is not None:
+            # [3.] [ ❌, ✅, ❌ ]
+            ngpus = nhosts * ngpu_per_host_max
+            ngpu_per_host = ngpus // nhosts
+        else:
+            # [5.] [ ❌, ✅, ✅ ]
+            assert nhosts is not None and ngpu_per_host is not None
+            ngpus = nhosts * ngpu_per_host
+    else:
+        if nhosts is not None and ngpu_per_host is None:
+            # [7.] [ ✅, ✅, ❌ ]
             assert ngpus % nhosts == 0, (
                 f"`ngpus` must be divisible by `nhosts`: {ngpus=} vs. {nhosts=}"
             )
             ngpu_per_host = ngpus // nhosts
-        elif ngpu_per_host is not None and nhosts is None:
-            assert ngpus % ngpu_per_host == 0, (
-                f"`ngpus` must be divisible by `ngpu_per_host`: "
-                f"{ngpus=} vs. {ngpu_per_host=}"
-            )
-            nhosts = ngpus // ngpu_per_host
-        elif ngpu_per_host is None and nhosts is None:
-            ngpus = ngpus or ngpus_max
-            ngpu_per_host = ngpu_per_host or ngpu_per_host_max
-            nhosts = ngpus // ngpu_per_host
         else:
+            # [8.] [ ✅, ✅, ✅ ]
             assert nhosts is not None and ngpu_per_host is not None
             assert ngpus == (nhosts * ngpu_per_host), (
                 f"Mismatch in `ngpus` and `nhosts * ngpu_per_host`: "
                 f"{ngpus=} vs. {nhosts=} * {ngpu_per_host=}"
             )
-    else:
-        ngpus = ngpus or ngpus_max
-        ngpu_per_host = ngpu_per_host or ngpu_per_host_max
 
-        _nh = ezpz.get_num_nodes(hostfile=Path(hostfile))
-        assert ngpus is not None and ngpu_per_host is not None
-        _nhosts = nhosts or (ngpus // ngpu_per_host)
-        assert _nh == _nhosts, (
-            f"Mismatch in `nhosts` and number of hosts in `hostfile`: "
-            f"{_nhosts=} vs. {_nh=}"
-        )
-        nhosts = _nhosts
+    # if ngpus is None and ngpu_per_host is not None:
+    #     ngpus = ngpu_per_host * ezpz.get_num_nodes(hostfile=Path(hostfile))
+    # elif ngpus is None and ngpu_per_host is None:
+    #     ngpus = ngpus_max
+    #     ngpu_per_host = ngpu_per_host_max
+    # elif ngpus is not None:
+    #     # [Scenario 1]:
+    #     if ngpu_per_host is None and nhosts is not None:
+    #         assert ngpus % nhosts == 0, (
+    #             f"`ngpus` must be divisible by `nhosts`: {ngpus=} vs. {nhosts=}"
+    #         )
+    #         ngpu_per_host = ngpus // nhosts
+    #     elif ngpu_per_host is not None and nhosts is None:
+    #         assert ngpus % ngpu_per_host == 0, (
+    #             f"`ngpus` must be divisible by `ngpu_per_host`: "
+    #             f"{ngpus=} vs. {ngpu_per_host=}"
+    #         )
+    #         nhosts = ngpus // ngpu_per_host
+    #     elif ngpu_per_host is None and nhosts is None:
+    #         ngpus = ngpus or ngpus_max
+    #         ngpu_per_host = ngpu_per_host or ngpu_per_host_max
+    #         nhosts = ngpus // ngpu_per_host
+    #     else:
+    #         assert nhosts is not None and ngpu_per_host is not None
+    #         assert ngpus == (nhosts * ngpu_per_host), (
+    #             f"Mismatch in `ngpus` and `nhosts * ngpu_per_host`: "
+    #             f"{ngpus=} vs. {nhosts=} * {ngpu_per_host=}"
+    #         )
+    # else:
+    #     ngpus = ngpus or ngpus_max
+    #     ngpu_per_host = ngpu_per_host or ngpu_per_host_max
+    #
+    #     _nh = ezpz.get_num_nodes(hostfile=Path(hostfile))
+    #     assert ngpus is not None and ngpu_per_host is not None
+    #     _nhosts = nhosts or (ngpus // ngpu_per_host)
+    #     assert _nh == _nhosts, (
+    #         f"Mismatch in `nhosts` and number of hosts in `hostfile`: "
+    #         f"{_nhosts=} vs. {_nh=}"
+    #     )
+    #     nhosts = _nhosts
 
     assert 0 < ngpus <= ngpus_max, (
         f"`ngpus` must be > 0 and <= {ngpus_max}: {ngpus=}"
     )
+    emoji = "✅" if ngpus == ngpus_max else "⚠️"
     logger.info(
-        f"Using [{ngpus:>}/{ngpus_max:<}] GPUs [{nhosts} hosts] x [{ngpu_per_host} GPU/host]"
+        f"{emoji} Using [{ngpus:>}/{ngpus_max:<}] GPUs [{nhosts} hosts] x [{ngpu_per_host} GPU/host]"
     )
     if ngpus != ngpus_max:
         logger.warning(
-            f"[WARNING] Using only [{ngpus:>}/{ngpus_max:<}] available GPUs!!"
+            f"[🚧 WARNING] Using only [{ngpus:>}/{ngpus_max:<}] available GPUs!!"
         )
-    # ncpus_per_host = get_cpus_per_node()
     if ezpz.get_machine().lower() == "sophia":
         return " ".join(
             [
@@ -211,7 +248,6 @@ def get_pbs_launch_cmd(
             "mpiexec",
             "--verbose",
             "--envall",
-            # f'-n {ngpus}',
             f"--np={ngpus}",
             f"--ppn={ngpu_per_host}",
             f"--hostfile={hostfile}",
@@ -256,38 +292,6 @@ def get_running_jobs_from_qstat() -> list[int]:
     ]
 
 
-# def get_pbs_jobid_from_qstat() -> int:
-#     """Get the PBS job ID from qstat"""
-#     from ezpz.configs import get_scheduler
-#
-#     assert get_scheduler() == "PBS"
-#     try:
-#         from sh import qstat as sh_qstat  # pyright:ignore
-#     except Exception as exc:
-#         raise exc
-#     qstat_out = sh_qstat("-u", os.environ.get("USER")).split("\n")[2:-1]
-#     return int(qstat_out[-1].split(".")[0])
-#     # except Exception as exc:
-#     #     logger.error('Unable to determine PBS_JOBID from `qstat` command...')
-#     #     raise exc
-
-
-# def get_pbs_nodefile_from_qstat() -> Path:
-#     """Get the PBS nodefile from qstat"""
-#     from ezpz.configs import get_scheduler
-#
-#     assert get_scheduler() == "PBS"
-#     nodefile = os.environ.get("PBS_NODEFILE", None)
-#     if nodefile is not None and (nf := Path(nodefile)).is_file():
-#         return nf
-#     pbs_jobid = get_pbs_jobid_from_qstat()
-#     matches = [
-#         i for i in Path("/var/spool/pbs/aux/").rglob(f"*{pbs_jobid}*") if i.is_file()
-#     ]
-#     assert len(matches) == 1
-#     return matches[0]
-
-
 def get_pbs_launch_info(
     hostfile: Optional[str | Path] = None,
     jobid: Optional[int | str] = None,
@@ -298,37 +302,15 @@ def get_pbs_launch_info(
     assert get_scheduler() == "PBS"
     if hostfile is None:
         hostfile = get_pbs_nodefile(jobid=jobid)
-        # hostfile = os.environ.get("PBS_NODEFILE", ezpz.get_pbs_nodefile_from_qstat())
     assert hostfile is not None
     hfp = Path(hostfile)
-    # hostfile = os.environ.get("PBS_NODEFILE", None)
-    # if hostfile is None:
-    #     hostfile = (
-    #             get_pbs_nodefile_from_qstat() if hostfile is None else
-    #             Path(hostfile)
-    #     )
-    # assert hostfile is not None
-    # hf = Path(hostfile)
-    # assert hostfile is not None and hf.is_file()
-    # hfp = Path(hostfile)
     hosts = ezpz.dist.get_nodes_from_hostfile(hfp)
     hosts = [h.split(".")[0] for h in hosts]
     nhosts = len(hosts)
     ngpu_per_host = ezpz.dist.get_gpus_per_node()
-    # ngpus = nhosts * ngpu_per_host
     ngpus_available = ezpz.dist.get_world_size(total=True)
     ngpus = nhosts * ngpu_per_host
     world_size_total = ezpz.dist.get_world_size_total()
-    # if ngpus != world_size_total:
-    #     logger.warning('Disagreement in total world size!!')
-    #     logger.warning(' '.join([
-    #         f'{get_world_size(total=True)=}',
-    #         f' vs. {get_world_size_total()=}'
-    #     ]))
-    #     logger.warning(' '.join([
-    #         'Mismatch in: ',
-    #         f'{ngpus=} vs. {ngpu_per_host=} * {nhosts=}'
-    #     ]))
     launch_cmd = get_pbs_launch_cmd(hostfile=hostfile)
     return {
         "HOSTFILE": hfp.as_posix(),
@@ -370,18 +352,38 @@ def get_pbs_env(
         pbsenv |= {"LAUNCH_CMD": get_pbs_launch_cmd(hostfile=hostfile)}
     os.environ |= pbsenv
     if verbose and ezpz.dist.get_rank() == 0:
-        # logger.debug(f'pbsenv={json.dumps(pbsenv, indent=4, sort_keys=True)}')
         ezpz.dist.log_dict_as_bulleted_list(pbsenv, name="pbsenv")
     return pbsenv
 
 
-def build_launch_cmd(hostfile: Optional[Union[str, Path, os.PathLike]] = None) -> str:
+def build_launch_cmd(
+    ngpus: Optional[int] = None,
+    nhosts: Optional[int] = None,
+    ngpu_per_host: Optional[int] = None,
+    hostfile: Optional[Union[str, Path, os.PathLike]] = None,
+) -> str:
     """Build the launch command for the current job.
 
     Returns:
         str: The launch command.
     """
-    return get_pbs_launch_cmd(hostfile=hostfile)
+    from ezpz.configs import get_scheduler
+
+    scheduler = get_scheduler().lower()
+    if scheduler == "pbs":
+        return get_pbs_launch_cmd(
+            ngpus=ngpus,
+            nhosts=nhosts,
+            ngpu_per_host=ngpu_per_host,
+            hostfile=hostfile,
+        )
+
+    elif scheduler == "slurm":
+        import ezpz.slurm
+
+        return ezpz.slurm.build_launch_cmd()
+    else:
+        raise ValueError(f"Unsupported scheduler: {scheduler}")
 
 
 if __name__ == "__main__":
