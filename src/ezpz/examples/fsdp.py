3200dc4b6ee961ce50fb33240dfa440af1412ea4
Sam Foreman <saforem2@gmail.com>
2025-12-13 09:03:02 -0600

    chore: Update `src/ezpz/examples/fsdp.py`

--- src/ezpz/examples/fsdp.py
+++ src/ezpz/examples/fsdp.py
@@ -22,37 +22,29 @@ from torch.optim.lr_scheduler import StepLR
 
 from ezpz.models import summarize_model
 
-
 logger = ezpz.get_logger(__name__)
+
+try:
+    import wandb
+except Exception:
+    wandb = None  # type: ignore
+
 
 fp = Path(__file__)
 fname = f"{fp.parent.stem}.{fp.stem}"
-
-
-# logger.setLevel('INFO') if RANK == 0 else logger.setLevel('CRITICAL')
-#
-try:
-    import wandb
-
-    WANDB_DISABLED = os.environ.get("WANDB_DISABLED", False)
-except Exception:
-    wandb = None
-    WANDB_DISABLED = True
-
-
-if torch.cuda.is_available():
-    torch.cuda.set_device(ezpz.get_local_rank())
+WBPROJ_NAME = f"ezpz.{fp.parent.stem}.{fp.stem}"
+OUTPUT_DIR = Path(os.getcwd()).joinpath("outputs", fname)
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes: int = 10):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -79,6 +71,7 @@ def train(model, train_loader, optimizer, epoch, sampler=None):
         sampler.set_epoch(epoch)
     ezpz.dist.synchronize()
     t0 = time.perf_counter()
+    batch, target = next(iter(train_loader))
     for _, (batch, target) in enumerate(train_loader):
         batch, target = batch.to(DEVICE_ID), target.to(DEVICE_ID)
         optimizer.zero_grad()
@@ -90,8 +83,7 @@ def train(model, train_loader, optimizer, epoch, sampler=None):
         ddp_loss[1] += len(batch)
     ezpz.dist.synchronize()
     t1 = time.perf_counter()
-
-    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
+    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)  # type:ignore
     return {
         "epoch": epoch,
         "dt": t1 - t0,
@@ -118,7 +110,7 @@ def test(model, test_loader):
             ddp_loss[1] += pred.eq(target.view_as(pred)).sum().item()
             ddp_loss[2] += len(batch)
 
-    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
+    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)  # type:ignore
 
     test_loss = ddp_loss[0] / ddp_loss[2]
 
@@ -131,7 +123,17 @@ def test(model, test_loader):
 def prepare_model_optimizer_and_scheduler(args: argparse.Namespace) -> dict:
     DEVICE = ezpz.get_torch_device()
     DEVICE_ID = f"{DEVICE}:{ezpz.get_local_rank()}"
-    model = Net().to(DEVICE_ID)
+    if args.dataset == "MNIST":
+        num_classes = 10
+    elif args.dataset == "OpenImages":
+        num_classes = 600
+    elif args.dataset == "ImageNet":
+        num_classes = 1000
+    elif args.dataset == "ImageNet1k":
+        num_classes = 1000
+    else:
+        raise ValueError(f"Unsupported dataset: {args.dataset}")
+    model = Net(num_classes=num_classes).to(DEVICE_ID)
     logger.info(f"\n{summarize_model(model, verbose=False, depth=2)}")
     dtypes = {
         "fp16": torch.float16,
@@ -140,7 +142,6 @@ def prepare_model_optimizer_and_scheduler(args: argparse.Namespace) -> dict:
         "fp32": torch.float32,
     }
     dtype = dtypes[args.dtype]
-    # if args.dtype in {'fp16', 'bf16', 'fp32'}:
     model = FSDP(
         model,
         mixed_precision=MixedPrecision(
@@ -148,8 +149,6 @@ def prepare_model_optimizer_and_scheduler(args: argparse.Namespace) -> dict:
             cast_forward_inputs=True,
         ),
     )
-    # else:
-    #     model = FSDP(model)
     optimizer = optim.AdamW(model.parameters(), lr=args.lr)
     logger.info(f"{model=}")
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
@@ -160,39 +159,91 @@ def prepare_model_optimizer_and_scheduler(args: argparse.Namespace) -> dict:
     }
 
 
-def fsdp_main(args: argparse.Namespace) -> None:
-    from ezpz.data.vision import get_mnist
-
-    rank = ezpz.setup_torch(
-        backend=os.environ.get("BACKEND", "DDP"),
-    )
-    # DEVICE = ezpz.get_torch_device()
-    if ezpz.get_rank() == 0 and not os.environ.get("WANDB_DISABLED", False):
-        try:
-            import wandb
-        except Exception as e:
-            logger.exception("Failed to import wandb")
-            raise e
-        fp = Path(__file__)
-        run = ezpz.setup_wandb(project_name=f"ezpz.{fp.parent.stem}.{fp.stem}")
-        assert run is not None and run is wandb.run
-        wandb.config.update({**vars(args)})
-        wandb.config.update(ezpz.get_dist_info())
-
-    # data = prepare_data()
-
+def get_data(args: argparse.Namespace) -> dict:
+    # data_prefix_fallback = Path(os.getcwd()).joinpath(
+    #     ".cache", "ezpz", "data", f"{args.dataset.lower()}"
+    # )
+    # data_prefix = args.data_prefix or data_prefix_fallback
+    # if args.dataset == "MNIST":
+    #     from ezpz.data.vision import get_mnist
+    #
+    #     data = get_mnist(
+    #         outdir=Path(data_prefix),
+    #         train_batch_size=args.batch_size,
+    #         test_batch_size=args.test_batch_size,
+    #         pin_memory=True,
+    #         num_workers=args.num_workers,
+    #     )
+    # else:
+    #     raise ValueError(f"Unsupported dataset: {args.dataset}")
     data_prefix_fallback = Path(os.getcwd()).joinpath(
-        ".cache", "ezpz", "data", "MNIST"
+        ".cache", "ezpz", "data", f"{args.dataset.lower()}"
     )
     data_prefix = args.data_prefix or data_prefix_fallback
-    data = get_mnist(
-        outdir=Path(data_prefix),
-        train_batch_size=args.batch_size,
-        test_batch_size=args.test_batch_size,
-        shuffle=False,
-        pin_memory=True,
-        num_workers=1,
-    )
+    if args.dataset == "MNIST":
+        from ezpz.data.vision import get_mnist
+
+        data = get_mnist(
+            outdir=Path(data_prefix),
+            train_batch_size=args.batch_size,
+            test_batch_size=args.test_batch_size,
+            pin_memory=True,
+            num_workers=args.num_workers,
+        )
+
+    elif args.dataset == "ImageNet1k":
+        from ezpz.data.vision import get_imagenet1k
+
+        data = get_imagenet1k(
+            outdir=Path(data_prefix),
+            train_batch_size=args.batch_size,
+            test_batch_size=args.test_batch_size,
+            pin_memory=True,
+            num_workers=args.num_workers,
+        )
+
+    elif args.dataset == "OpenImages":
+        from ezpz.data.vision import get_openimages
+
+        data = get_openimages(
+            outdir=Path(data_prefix),
+            train_batch_size=args.batch_size,
+            test_batch_size=args.test_batch_size,
+            shuffle=False,
+            pin_memory=True,
+            num_workers=args.num_workers,
+        )
+    elif args.dataset == "ImageNet":
+        from ezpz.data.vision import get_imagenet
+
+        data = get_imagenet(
+            outdir=Path(data_prefix),
+            train_batch_size=args.batch_size,
+            test_batch_size=args.test_batch_size,
+            shuffle=False,
+            pin_memory=True,
+            num_workers=args.num_workers,
+        )
+    else:
+        raise ValueError(f"Unsupported dataset: {args.dataset}")
+
+    return data
+
+
+def fsdp_main(args: argparse.Namespace) -> None:
+    rank = ezpz.setup_torch()
+    START_TIME = ezpz.get_timestamp() if ezpz.get_rank() == 0 else None
+    START_TIME = ezpz.dist.broadcast(START_TIME, root=0)
+    if rank == 0:
+        # try:
+        fp = Path(__file__)
+        run = ezpz.setup_wandb(project_name=f"ezpz.{fp.parent.stem}.{fp.stem}")
+        if run is not None and wandb is not None and run is wandb.run:
+            run.config.update({**vars(args)})
+            run.config.update({"ezpz.dist": {**ezpz.get_dist_info()}})
+
+    data = get_data(args)
+    ezpz.dist.barrier()
     train_loader = data["train"]["loader"]
     test_loader = data["test"]["loader"]
 
@@ -201,7 +252,19 @@ def fsdp_main(args: argparse.Namespace) -> None:
     optimizer = tmp["optimizer"]
     scheduler = tmp["scheduler"]
 
-    history = ezpz.History()
+    # if rank == 0:
+    outdir = Path(os.getcwd()).joinpath("outputs", fname, START_TIME)
+    metrics_path = outdir.joinpath(f"metrics-{rank}.jsonl")
+    outdir.mkdir(parents=True, exist_ok=True)
+    history = ezpz.history.History(
+        report_dir=outdir,
+        report_enabled=(rank == 0),
+        jsonl_path=metrics_path,
+        # jsonl_overwrite=True,
+        distributed_history=(
+            1 < ezpz.get_world_size() <= 384  # and not config.pytorch_profiler
+        ),
+    )
     start = time.perf_counter()
     for epoch in range(1, args.epochs + 1):
         train_metrics = train(
@@ -223,10 +286,10 @@ def fsdp_main(args: argparse.Namespace) -> None:
             ]
         )
     )
-    dist.barrier()
+    ezpz.dist.barrier()
 
     if args.save_model:
-        dist.barrier()  # wait for slowpokes
+        ezpz.dist.barrier()  # wait for slowpokes
         states = model.state_dict()
         if rank == 0:
             torch.save(states, "mnist_cnn.pt")
@@ -239,6 +302,20 @@ def fsdp_main(args: argparse.Namespace) -> None:
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="PyTorch MNIST Example using FSDP"
+    )
+    parser.add_argument(
+        "--num-workers",
+        type=int,
+        default=4,
+        metavar="N",
+        help="number of data loading workers (default: 4)",
+    )
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="MNIST",
+        choices=["MNIST", "OpenImages", "ImageNet", "ImageNet1k"],
+        help="Dataset to use (default: MNIST)",
     )
     parser.add_argument(
         "--batch-size",
