768ef606a28048b7abbf7e57421b5555ae6d40b5
Sam Foreman <saforem2@gmail.com>
2026-01-31 11:04:15 -0600

    chore: Update `src/ezpz/dist.py`

--- src/ezpz/dist.py
+++ src/ezpz/dist.py
@@ -1202,6 +1202,21 @@ def get_local_rank() -> int:
             ),
         ),
     )
+    if not os.environ.get("EZPZ_LOCAL_RANK_DEBUG_LOGGED"):
+        os.environ["EZPZ_LOCAL_RANK_DEBUG_LOGGED"] = "1"
+        logger.info(
+            "Local rank envs on %s rank=%s: LOCAL_RANK=%s PMI_LOCAL_RANK=%s "
+            "OMPI_COMM_WORLD_LOCAL_RANK=%s MPI_LOCALRANKID=%s MPICH_LOCALRANKID=%s "
+            "SLURM_LOCAL_ID=%s",
+            get_hostname(),
+            get_rank(),
+            os.environ.get("LOCAL_RANK"),
+            os.environ.get("PMI_LOCAL_RANK"),
+            os.environ.get("OMPI_COMM_WORLD_LOCAL_RANK"),
+            os.environ.get("MPI_LOCALRANKID"),
+            os.environ.get("MPICH_LOCALRANKID"),
+            os.environ.get("SLURM_LOCAL_ID"),
+        )
     if local_rank is not None:
         return int(local_rank)
     if get_rank() == 0 and not os.environ.get("EZPZ_LOCAL_RANK_LOGGED"):
